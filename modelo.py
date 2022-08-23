da446facc9d9a6af490e0d866279f2ab023401c2
oliveira-ro <gemes432@gmail.com>
2022-08-23 20:35:23 -0300

    att

--- modelo.py
+++ modelo.py
@@ -1,3 +1,6 @@
+
+
+
 class Programa:
     def __init__(self, nome, ano):
         self._nome = nome.title()
@@ -43,16 +46,39 @@ class Serie(Programa):
     def __str__(self):
         return f'{self._nome} - {self.ano} - {self.temporadas} Temporadas - Likes: {self._likes}'
 
-
+#herda para a classe a função list
+class Playlist(list):
+    def __init__(self, nome, programas):
+        super().__init__(programas)
+        self.nome = nome
+        self.programas = programas
+    
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
+thor = Filme('thor - love and thunder', 2022, 130)
+lucifer = Serie('lucifer', 2015, 6)
 suits = Serie('suits', 2018, 9)
+
+thor.dar_likes()
+thor.dar_likes()
+thor.dar_likes()
+lucifer.dar_likes()
 suits.dar_likes()
+suits.dar_likes()
+lucifer.dar_likes()
 vingadores.dar_likes()
+vingadores.dar_likes()
+vingadores.dar_likes()
+
 # print(f'Nome: {vingadores.nome} - Likes: {vingadores.likes}')
 # print(f'Nome: {suits.nome} - Likes: {suits.likes}')
 
-filmes_e_series = [vingadores, suits]
+filmes_e_series = [vingadores, suits, lucifer, thor]
+playlist_fds = Playlist('fim de semana', filmes_e_series)
 
-for programa in filmes_e_series:
+print(f'Tamanho da playlist: {len(playlist_fds)}')
+
+for programa in playlist_fds:
     print(programa)
+
+print(f'Ta ou não ta? {thor in playlist_fds}')
