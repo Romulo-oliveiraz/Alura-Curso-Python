77dee93f65719a7e8f7b6c4081f0cc2cd304775e
lapagus <gemes432@gmail.com>
2022-08-28 04:16:53 -0300

    STR 2

--- strings curso/extrator-url.py
+++ strings curso/extrator-url.py
@@ -1,26 +1,34 @@
 class ExtratorUrl:
     def __init__(self, url):
+        """Salva a url em um atributo do objeto (self.url = url) e verifica se a url é válida"""
         self.url = self.sanitiza_url(url)
         self.valida_url()
     
     def sanitiza_url(self, url):
+        """Retorna a url removendo espaços em branco."""
         if type(url) == str:
             return url.strip()
         else:
             return ""
     def valida_url(self):
+        """Valida se a url está vazia"""
         if not self.url:
             raise ValueError("URL não pode ser vazia")
+        if not self.url.startswith("http") or not self.url.startswith("https"):
+            raise ValueError("URL precisa ser do tipo http ou https")
 
     def get_url_base(self):
+        """Retorna a base da url."""
         #procura ate o primeiro caracter '?'
         return self.url[:self.url.find('?')]
     
     def get_url_parametros(self):
+        """Retorna os parâmetros da url."""
         #procura a partir do primeiro caracter '?'
         return self.url[self.url.find('?')+1:]
     
     def get_valor_parametros(self, nome_parametro):
+        """Retorna o valor do parametro `parametro_busca`."""
         indiceProcurado = nome_parametro
         #procura a posição do caractere ?
         #verifica onde na string o parametro do indiceProcurado está e pega a posição final dele.
@@ -36,7 +44,7 @@ class ExtratorUrl:
         return valor
 
 
-extrator_url = ExtratorUrl("    https://bytebank.com/cambio?moedaDestino=dolar&moedaOrigem=real")
+extrator_url = ExtratorUrl("https://bytebank.com/cambio?moedaDestino=dolar&moedaOrigem=real")
 
 v = extrator_url.get_url_base()
 c = extrator_url.get_url_parametros()
