e2568b5776c9a4fa669c191c74194935e8bb84b1
Romulo.oliveiraz <gemes432@gmail.com>
2022-09-05 03:21:16 -0300

    att

--- calculadora/main.py
+++ calculadora/main.py
@@ -1,37 +1,46 @@
 
 import PySimpleGUI as sg
 def janela1():
-    sg.theme('Reddit')
-    layout = [  
-        [sg.Txt(''  * 10)],         
-        [sg.Text('', size=(15, 2), font=('Helvetica', 18), text_color='red', key='input')],
-        [sg.Txt(''  * 10)],
-        [sg.ReadFormButton('%', size=(5, 2)), sg.ReadFormButton('CE', size=(5, 2)), sg.ReadFormButton('C', size=(5, 2)), sg.ReadFormButton('<<', size=(5, 2))],
-        [sg.ReadFormButton('7', size=(5,2)), sg.ReadFormButton('8', size=(5,2)), sg.ReadFormButton('9', size=(5,2)), sg.ReadFormButton('/', size=(5,2))],
-        [sg.ReadFormButton('4', size=(5,2)), sg.ReadFormButton('5', size=(5,2)), sg.ReadFormButton('6', size=(5,2)), sg.ReadFormButton('x', size=(5,2))],
-        [sg.ReadFormButton('1', size=(5,2)), sg.ReadFormButton('2', size=(5,2)), sg.ReadFormButton('3', size=(5,2)), sg.ReadFormButton('-', size=(5,2))],
-        [sg.ReadFormButton('.', size=(5,2)), sg.ReadFormButton('0', size=(5,2)), sg.ReadFormButton('=', size=(5,2)), sg.ReadFormButton('+', size=(5,2))],    
+
+
+    base_num = {'size':(6,2), 'font':('Impact', 14), 'button_color':('white', '#000000')}
+    base_op = {'size':(6,2), 'font':('Impact', 14), 'button_color':('white', '#013445')}
+
+
+    layout = [         
+        [sg.Text('', size=(20, 1), font=('Impact', 21), text_color=('white'), background_color='#737373', key='input')],
+        [sg.ReadFormButton('%',**base_op), sg.ReadFormButton('CE', **base_op), sg.ReadFormButton('C', **base_op), sg.ReadFormButton('<<',**base_op)],
+        [sg.ReadFormButton('7',**base_num), sg.ReadFormButton('8', **base_num), sg.ReadFormButton('9', **base_num), sg.ReadFormButton('/', **base_op)],
+        [sg.ReadFormButton('4',**base_num), sg.ReadFormButton('5',**base_num), sg.ReadFormButton('6',**base_num), sg.ReadFormButton('x',**base_op)],
+        [sg.ReadFormButton('1',**base_num), sg.ReadFormButton('2',**base_num), sg.ReadFormButton('3',**base_num), sg.ReadFormButton('-',**base_op)],
+        [sg.ReadFormButton('.',**base_op), sg.ReadFormButton('0',**base_num), sg.ReadFormButton('=',**base_op), sg.ReadFormButton('+',**base_op)],    
     ]
     
     return layout
         # Janela
-form = sg.FlexForm('Calculadora', default_element_size=(40, 1))
+form = sg.FlexForm('Calculadora', default_element_size=(40, 1), background_color='#29000d', auto_size_buttons=False, grab_anywhere=False)
 form.Layout(janela1())
 
 equal = ''
+res = ''
 list_op = ['+', '-', 'x', '/']
 while True:
     button, value = form.read()
     if button == sg.WIN_CLOSED:
         break
-    if button is 'C':
+    if button == 'C':
         equal = ''
-        form.FindElement('input').Update(equal)
-    elif button is '<<':
+        res = ''
+        form.find_element('input').Update(equal)
+        form.find_element('input').Update(res)
+    elif button == 'CE':
+        equal = ''
+        form.find_element('input').Update(equal)
+    elif button == '<<':
         equal = equal[:-1]
-        form.FindElement('input').Update(equal)
+        form.find_element('input').Update(equal)
     elif str(button) in '0123456789.+-x/':
         equal += str(button)
-        form.FindElement('input').Update(equal)
+        form.find_element('input').Update(equal)
 
 
