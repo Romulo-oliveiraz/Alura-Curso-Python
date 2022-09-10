fad87257addf06696cf5f7d9b939ea18d319ae04
Romulo.oliveiraz <gemes432@gmail.com>
2022-09-10 16:04:05 -0300

    att

--- calculadora/main.py
+++ calculadora/main.py
@@ -7,7 +7,7 @@ def janela1():
 
 
     layout = [         
-        [sg.Text('', size=(20, 1), font=('Arial Black', 20), text_color=('white'), background_color='#737373', key='input')],
+        [sg.Text('', size=(17, 1), font=('Arial Black', 20), text_color=('white'), background_color='#737373', key='input')],
         [sg.ReadFormButton('(',**base_op), sg.ReadFormButton(')', **base_op), sg.ReadFormButton('C', **base_op), sg.ReadFormButton('<<',**base_op)],
         [sg.ReadFormButton('7',**base_num), sg.ReadFormButton('8', **base_num), sg.ReadFormButton('9', **base_num), sg.ReadFormButton('/', **base_op)],
         [sg.ReadFormButton('4',**base_num), sg.ReadFormButton('5',**base_num), sg.ReadFormButton('6',**base_num), sg.ReadFormButton('x',**base_op)],
@@ -23,6 +23,7 @@ form.Layout(janela1())
 
 equal = ''
 list_op = ['+', '-', 'x', '/', '*']
+list_op2 = ['x', '/', '*']
 
 while True:
     try:
@@ -30,6 +31,7 @@ while True:
 
         if button == sg.WIN_CLOSED:
             break
+
         #clean the numbers in te screen and in the operation
         if button == 'C':
             equal = ''
@@ -42,11 +44,14 @@ while True:
 
         #puts what was clicked on the screen and in the operation
 
-        elif str(button) in '0123456789.+-/x':
-            
-            if equal == '' and button in list_op:
+        elif str(button) in '0123456789.+-/x()':
+
+            #errors pass
+            if equal == '' and button in list_op2:
                 pass
             elif equal == '' and button == '.':
+                pass
+            elif equal == '' and button == ')':
                 pass
             else:
                 equal += str(button)
@@ -55,15 +60,17 @@ while True:
 
         #ERROR
         if equal != "":
-
-            if len(equal) > 1:    
+            
+            if len(equal) > 1:
+                #mutiple operators signals resolution
                 if equal[-1] in list_op and equal[-2] in list_op:
                     equal = equal[:-1] 
                     form.find_element('input').Update(equal)
+
+                #error when have a '.' in start of equal
                 elif equal[-1] == '.' and equal[-2] in list_op:
                     equal = equal[:-1] 
                     form.find_element('input').Update(equal)
-
         #result
         if button == '=' and equal != '':
 
@@ -72,15 +79,23 @@ while True:
                     if i in list_op and i in equal[0]:
                         equal = ""
                         form.find_element('input').Update(equal)
+                    elif i in ')' and equal.find('(') == -1:
+                        equal = equal.replace(')', '')
+                        form.find_element('input').Update(equal)
+                        print(equal)
+
                 #x sintaxe error 
                 if 'x' in equal:
                     equal = equal.replace('x', '*')
+
                 #last caracter operator symble error resolution
                 if equal[-1] not in list_op:
                     resolução = eval(equal)
                     history = equal+'='+str(resolução)
                     form.find_element('input').Update(resolução)
                     equal = str(resolução)
+
+                    #make a txt file with the history of results
                     with open("history.txt", "a") as history_txt:
                         history_txt.write(str(history))
                         history_txt.write('\n')
@@ -90,5 +105,11 @@ while True:
 
             except ZeroDivisionError:
                 sg.popup('Não é possivel dividir por 0!')
-    except:
-        sg.popup('ERROR')
+            except SyntaxError:
+                sg.popup('Error, Verifique os parenteses!')
+            except IndexError:
+                if resolução == 0:
+                    equal = '0'
+                    form.find_element('input').Update(equal)
+    except Exception as e:
+        print(e)
