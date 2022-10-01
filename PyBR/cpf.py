9540ec60afd4bf10fa3e2f5ddfb2250e69d84042
Romulo.oliveiraz <gemes432@gmail.com>
2022-10-01 17:47:19 -0300

    att

--- PyBR/cpf.py
+++ PyBR/cpf.py
@@ -1,9 +1,11 @@
-from main import Cpf
+from main import Documento
 
 # cpf1 = "15316264754"
 # new_cpf = "11111111111"
 
-cpf = Cpf("15316264754")
+cpf = Documento.cria_doc("15316264754")
 print(cpf)
+cnpj = Documento.cria_doc('35379838000112')
+print(cnpj)
 
 
--- PyBR/main.py
+++ PyBR/main.py
@@ -1,23 +1,53 @@
-from validate_docbr import CPF
+from validate_docbr import CPF, CNPJ
 
-class Cpf:
+class Documento:
+
+    @staticmethod
+    def cria_doc(documento):
+        if len(documento) == 11:
+            return DocCpf(documento)
+        elif len(documento) == 14:
+            return DocCnpj(documento)
+        else:
+            raise ValueError('Quantidade de digitos invalida!')
+#################################################################
+
+class DocCpf:
     def __init__(self, documento):
-        documento = str(documento)
         if self.cpf_valido(documento):
             self.cpf = documento
         else:
             raise ValueError("CPF inválido!")
-        
-    def cpf_valido(self, cpf):
-        if len(cpf) == 11:
-            validador = CPF()
-            return validador.validate(cpf)
-        else:
-            raise ValueError('Quantidade de digitos invalida!')
     
+    def cpf_valido(self, documento):
+        validador = CPF()
+        return validador.validate(documento)
+
     def formata_cpf(self):
         mascara = CPF()
         return mascara.mask(self.cpf)
 
     def __str__(self):
         return self.formata_cpf()
+#################################################################
+
+class DocCnpj:
+    def __init__(self, documento):
+        if self.cnpj_valido(documento):
+            self.cnpj = documento
+        else:
+            raise ValueError("CNPJ inválido!")
+    
+    def cnpj_valido(self, documento):
+        validador = CNPJ()
+        return validador.validate(documento)
+
+    def formata_cnpj(self):
+        mascara = CNPJ()
+        return mascara.mask(self.cnpj)
+
+    def __str__(self):
+        return self.formata_cnpj()
+#################################################################
+
+
